e6a04ce1fee2b3076a76ca316cf705b1daa969b0
Sampson <jjdsampson@gmail.com>
2025-08-26 21:04:56 -0500

    fixes linting issues

--- nodes/BraveSearch/operations/news/parameters.ts
+++ nodes/BraveSearch/operations/news/parameters.ts
@@ -32,44 +32,44 @@ parameters.push(
 // Optional Parameters
 optional_parameters.push(
 	{
-		name: 'country',
 		displayName: 'Country',
+		name: 'country',
 		type: 'options',
+		// eslint-disable-next-line n8n-nodes-base/node-param-default-wrong-for-options
 		default: 'US',
 		description:
 			'The search query country, where the results come from. The country string is limited to 2 character country codes of supported countries.',
-		options: country_codes.map((code) => ({
-			name: code.country,
-			value: code.code,
-		})),
+		options: country_codes.map(({ country, code }) => ({ name: country, value: code })),
 	},
 	{
-		name: 'search_lang',
 		displayName: 'Search Language',
+		name: 'search_lang',
 		type: 'options',
+		// eslint-disable-next-line n8n-nodes-base/node-param-default-wrong-for-options
 		default: 'en',
 		description:
 			'The language of the search query. The language string is limited to 2 character language codes of supported languages.',
-		options: language_codes.map((code) => ({
-			name: code.language,
-			value: code.code,
+		options: language_codes.map(({ language, code }) => ({
+			name: language,
+			value: code,
 		})),
 	},
 	{
-		name: 'ui_lang',
 		displayName: 'User Interface Language',
+		name: 'ui_lang',
 		type: 'options',
+		// eslint-disable-next-line n8n-nodes-base/node-param-default-wrong-for-options
 		default: 'en-US',
 		description:
 			'User interface language preferred in response. Usually of the format <language_code>-<country_code>. For more, see <a href="https://www.rfc-editor.org/rfc/rfc9110.html#name-accept-language">RFC 9110</a>.',
-		options: market_codes.map((code) => ({
-			name: `${code.language} (${code.country})`,
-			value: code.code,
+		options: market_codes.map(({ country, language, code }) => ({
+			name: `${language} (${country})`,
+			value: code,
 		})),
 	},
 	{
-		name: 'offset',
 		displayName: 'Offset',
+		name: 'offset',
 		type: 'number',
 		default: 0,
 		description:
@@ -80,39 +80,39 @@ optional_parameters.push(
 		},
 	},
 	{
-		name: 'spellcheck',
 		displayName: 'Spellcheck',
+		name: 'spellcheck',
 		type: 'boolean',
 		default: true,
 		description: 'Whether to enable spellcheck for the search query. The default is true.',
 	},
 	{
-		name: 'safesearch',
 		displayName: 'Safe Search',
+		name: 'safesearch',
 		type: 'options',
-		default: '',
+		default: 'moderate',
 		description: 'Whether to enable safe search for the search query. The default is moderate.',
 		options: [
 			{
-				name: 'off',
+				name: 'Off',
 				value: 'off',
 			},
 			{
-				name: 'moderate',
+				name: 'Moderate',
 				value: 'moderate',
 			},
 			{
-				name: 'strict',
+				name: 'Strict',
 				value: 'strict',
 			},
 		],
 	},
 	{
-		name: 'freshness',
 		displayName: 'Freshness',
+		name: 'freshness',
 		type: 'options',
 		default: '',
-		description: 'The freshness of the search query.',
+		description: 'The freshness of the search query',
 		options: [
 			{
 				name: 'All Time',
@@ -137,20 +137,20 @@ optional_parameters.push(
 		],
 	},
 	{
-		name: 'extra_snippets',
 		displayName: 'Extra Snippets',
+		name: 'extra_snippets',
 		type: 'boolean',
 		default: false,
 		description:
 			'Whether to enable extra snippets for the search query. A snippet is an excerpt from a page you get as a result of the query, and extra_snippets allow you to get up to 5 additional, alternative excerpts. Only available under Free AI, Base AI, Pro AI, Base Data, Pro Data and Custom plans.',
 	},
 	{
-		name: 'goggles',
 		displayName: 'Goggles',
+		name: 'goggles',
 		type: 'string',
 		default: '',
 		description:
-			'Goggles act as a custom re-ranking on top of Brave’s search index. The parameter supports both a url where the Goggle is hosted or the definition of the Goggle. For more details, refer to the <a href="https://github.com/brave/goggles-quickstart">Goggles repository</a>. The parameter can be repeated to query with multiple goggles.',
+			'Goggles act as a custom re-ranking on top of Brave’s search index. The parameter supports both a URL where the Goggle is hosted or the definition of the Goggle. For more details, refer to the <a href="https://github.com/brave/goggles-quickstart">Goggles repository</a>. The parameter can be repeated to query with multiple goggles.',
 	},
 );
 
